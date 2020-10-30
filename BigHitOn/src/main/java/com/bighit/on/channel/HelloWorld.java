0070a052f675a916933c4ad62cb83089cc9aa624
isc <isc@DESKTOP-C3NCPKE>
2020-10-30 16:39:47 +0900

    delete

--- BigHitOn/src/main/java/com/bighit/on/channel/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.bighit.on.channel;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("Hello World!");
-	}
-}
