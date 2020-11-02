b8ebfa841045317e91b523fcf7574b69db666a94
rladu <rladu@LAPTOP-0G4AK664>
2020-11-02 13:38:38 +0900

    계정 다오 pull

---
+++ BigHitOn/src/main/java/com/bighit/on/user/dao/UsersDaoImpl.java
@@ -0,0 +1,242 @@
+package com.bighit.on.user.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+
+@Repository("UsersDaoImpl")
+public class UsersDaoImpl {
+	 final static Logger   LOG = LoggerFactory.getLogger(UsersDaoImpl.class);
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	RowMapper rowMapper = new RowMapper<UsersVO>() {
+
+		@Override
+		public UsersVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			UsersVO outVO = new UsersVO();
+			
+			outVO.setUser_serial(rs.getString("user_serial"));
+			outVO.setWs_link(rs.getString("ws_link"));
+			outVO.setEmail(rs.getString("email"));
+			outVO.setPassword(rs.getString("password"));
+			outVO.setName(rs.getString("name"));
+			outVO.setNickname(rs.getString("nickname"));
+			outVO.setProfile_img(rs.getString("profile_img"));
+			outVO.setPosition(rs.getString("position"));
+			outVO.setPhone_num(rs.getString("phone_num"));
+			outVO.setCountry(rs.getInt("country"));
+			outVO.setState(rs.getInt("state"));
+			outVO.setOnline_state(rs.getInt("online_state"));
+			outVO.setReg_id(rs.getString("reg_id"));
+			outVO.setReg_dt(rs.getString("reg_dt"));
+			
+			return outVO;
+			
+		}
+		
+	};
+	
+	public UsersDaoImpl() {}
+	
+	public List<UsersVO> doSelectList(UsersVO usersVO){
+		List<UsersVO> list  = null;
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append(" SELECT                   \n");
+		sb.append("     user_serial,         \n");
+		sb.append("     ws_link,             \n");
+		sb.append("     email,               \n");
+		sb.append("     password,            \n");
+		sb.append("     name,                \n");
+		sb.append("     nickname,            \n");
+		sb.append("     profile_img,         \n");
+		sb.append("     position,            \n");
+		sb.append("     phone_num,           \n");
+		sb.append("     country,             \n");
+		sb.append("     state,               \n");
+		sb.append("     online_state,        \n");
+		sb.append("     reg_id,              \n");
+		sb.append("     reg_dt               \n");
+		sb.append(" FROM users               \n");
+		sb.append(" WHERE ws_link = ?        \n");
+		LOG.debug("========================");
+		LOG.debug("=sql\n="+sb.toString());
+		LOG.debug("=param="+usersVO);
+		LOG.debug("========================");
+		
+		list = this.jdbcTemplate.query(sb.toString(), new Object[] {"%"+usersVO.getWs_link()+"%"}, rowMapper);
+		for(UsersVO vo:list) {
+			LOG.debug("====================================");
+			LOG.debug("=vo="+vo);
+			LOG.debug("====================================");
+		}
+		
+		return list;
+				
+	}
+	
+	public UsersVO doSelectOne(String userSerial) throws Exception{
+		UsersVO outVO = null; 
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" SELECT                   \n");
+		sb.append("     user_serial,         \n");
+		sb.append("     ws_link,             \n");
+		sb.append("     email,               \n");
+		sb.append("     password,            \n");
+		sb.append("     name,                \n");
+		sb.append("     nickname,            \n");
+		sb.append("     profile_img,         \n");
+		sb.append("     position,            \n");
+		sb.append("     phone_num,           \n");
+		sb.append("     country,             \n");
+		sb.append("     state,               \n");
+		sb.append("     online_state,        \n");
+		sb.append("     reg_id,              \n");
+		sb.append("     reg_dt               \n");
+		sb.append(" FROM users               \n");
+		sb.append(" WHERE user_serial = ?    \n");
+		LOG.debug("========================");
+		LOG.debug("=sql="+sb.toString());
+		LOG.debug("=param="+userSerial);
+		LOG.debug("========================");
+		
+		Object[] args = {userSerial};
+		outVO = (UsersVO) this.jdbcTemplate.queryForObject(sb.toString(), args, rowMapper);
+		LOG.debug("outVO = "+outVO);
+		
+		return outVO;
+	}
+	
+	public int doUpdate(UsersVO usersVO) {
+		int flag = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" UPDATE users              \n");
+		sb.append(" SET  email = ?            \n");
+		sb.append("     ,password = ?         \n");
+		sb.append("     ,name = ?             \n");
+		sb.append("     ,nickname = ?         \n");
+		sb.append("     ,profile_img = ?      \n");
+		sb.append("     ,position = ?         \n");
+		sb.append("     ,phone_num = ?        \n");
+		sb.append("     ,country = ?          \n");
+		sb.append("     ,state = ?            \n");
+		sb.append("     ,online_state = ?     \n");
+		sb.append(" WHERE user_serial = ?     \n");
+		LOG.debug("========================");
+		LOG.debug("=sql=\n"+sb.toString());
+		LOG.debug("=param="+usersVO);
+		LOG.debug("========================");
+		
+		Object[] args = { usersVO.getEmail(),
+						usersVO.getPassword(),
+						usersVO.getName(),
+						usersVO.getNickname(),
+						usersVO.getProfile_img(),
+						usersVO.getPosition(),
+						usersVO.getPhone_num(),
+						usersVO.getCountry(),
+						usersVO.getState(),
+						usersVO.getOnline_state(),
+						usersVO.getReg_id(),	
+						usersVO.getUser_serial()
+		};
+		
+		flag = this.jdbcTemplate.update(sb.toString(), args);
+		LOG.debug("=flag="+flag);
+		return flag;
+		
+	}
+	
+	public int doDelete(UsersVO usersVO) {
+		int flag = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" DELETE FROM users        \n");
+		sb.append(" WHERE user_serial = ?    \n");
+		
+		LOG.debug("=====================================");
+		LOG.debug("=sql=\n"+sb.toString());
+		LOG.debug("=param="+usersVO);
+		LOG.debug("=====================================");
+		
+		Object[] args = {usersVO.getUser_serial()};
+		flag = this.jdbcTemplate.update(sb.toString(),args);
+		LOG.debug("flag = "+flag);
+		
+		return flag;
+	}
+	
+	public int doInsert(UsersVO usersVO) {
+		int flag = 0;
+		
+		Object[] args = { 	usersVO.getUser_serial(),
+							usersVO.getWs_link(),
+							usersVO.getEmail(),
+							usersVO.getPassword(),
+							usersVO.getName(),
+							usersVO.getNickname(),
+							usersVO.getProfile_img(),
+							usersVO.getPosition(),
+							usersVO.getPhone_num(),
+							usersVO.getCountry(),
+							usersVO.getState(),
+							usersVO.getOnline_state(),
+							usersVO.getReg_id()	
+		};
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(" INSERT INTO users (      \n");
+		sb.append("     user_serial,         \n");
+		sb.append("     ws_link,             \n");
+		sb.append("     email,               \n");
+		sb.append("     password,            \n");
+		sb.append("     name,                \n");
+		sb.append("     nickname,            \n");
+		sb.append("     profile_img,         \n");
+		sb.append("     position,            \n");
+		sb.append("     phone_num,           \n");
+		sb.append("     country,             \n");
+		sb.append("     state,               \n");
+		sb.append("     online_state,        \n");
+		sb.append("     reg_id,              \n");
+		sb.append("     reg_dt               \n");
+		sb.append(" ) VALUES (               \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("     ?,                   \n");
+		sb.append("      SYSDATE             \n");
+		sb.append(" )                        \n");
+		LOG.debug("========================");
+		LOG.debug("=sql=\n"+sb.toString());
+		LOG.debug("=param="+usersVO);
+		LOG.debug("========================");
+		
+		flag= this.jdbcTemplate.update(sb.toString(), args);
+		LOG.debug("flag = "+flag);
+		
+		return flag;
+
+	}
+	
+}
---
+++ BigHitOn/src/main/java/com/bighit/on/user/dao/UsersVO.java
@@ -0,0 +1,165 @@
+package com.bighit.on.user.dao;
+
+public class UsersVO {
+	private String user_serial; /* 계정시리얼키 */
+	private String ws_link; /* 워크스페이스 링크 */
+	private String email; /* 이메일 */
+	private String password;  /* 비밀번호 */
+	private String name;  /* 이름 */
+	private String nickname;  /* 표시이름 */
+	private String profile_img;  /* 프로필사진 */
+	private String position;  /* 직책 */
+	private String phone_num;  /* 전화번호 */
+	private int country; /* 시간대 */
+	private int state;  /* 상태 */
+	private int online_state;  /* 상태(on/off) */
+	private String reg_id;  /* 작성자 */
+	private String reg_dt;  /* 작성시간 */
+	
+	public UsersVO() {
+		
+	}
+
+	public UsersVO(String user_serial, String ws_link, String email, String password, String name, String nickname,
+			String profile_img, String position, String phone_num, int country, int state, int online_state,
+			String reg_id, String reg_dt) {
+		super();
+		this.user_serial = user_serial;
+		this.ws_link = ws_link;
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.nickname = nickname;
+		this.profile_img = profile_img;
+		this.position = position;
+		this.phone_num = phone_num;
+		this.country = country;
+		this.state = state;
+		this.online_state = online_state;
+		this.reg_id = reg_id;
+		this.reg_dt = reg_dt;
+	}
+
+	public String getUser_serial() {
+		return user_serial;
+	}
+
+	public void setUser_serial(String user_serial) {
+		this.user_serial = user_serial;
+	}
+
+	public String getWs_link() {
+		return ws_link;
+	}
+
+	public void setWs_link(String ws_link) {
+		this.ws_link = ws_link;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getProfile_img() {
+		return profile_img;
+	}
+
+	public void setProfile_img(String profile_img) {
+		this.profile_img = profile_img;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getPhone_num() {
+		return phone_num;
+	}
+
+	public void setPhone_num(String phone_num) {
+		this.phone_num = phone_num;
+	}
+
+	public int getCountry() {
+		return country;
+	}
+
+	public void setCountry(int country) {
+		this.country = country;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public int getOnline_state() {
+		return online_state;
+	}
+
+	public void setOnline_state(int online_state) {
+		this.online_state = online_state;
+	}
+
+	public String getReg_id() {
+		return reg_id;
+	}
+
+	public void setReg_id(String reg_id) {
+		this.reg_id = reg_id;
+	}
+
+	public String getReg_dt() {
+		return reg_dt;
+	}
+
+	public void setReg_dt(String reg_dt) {
+		this.reg_dt = reg_dt;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_serial=" + user_serial + ", ws_link=" + ws_link + ", email=" + email + ", password="
+				+ password + ", name=" + name + ", nickname=" + nickname + ", profile_img=" + profile_img
+				+ ", position=" + position + ", phone_num=" + phone_num + ", country=" + country + ", state=" + state
+				+ ", online_state=" + online_state + ", reg_id=" + reg_id + ", reg_dt=" + reg_dt + ", toString()="
+				+ super.toString() + "]";
+	}
+	
+	
+}
