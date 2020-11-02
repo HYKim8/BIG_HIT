b69f92ca4ae68b254557e784a6a6935adbafe689
isc <isc@DESKTOP-C3NCPKE>
2020-11-02 16:37:58 +0900

    channeldao 단건,다건조회 - jhs

--- BigHitOn/src/main/java/com/bighit/on/channel/ChannelDaoImpl.java
+++ BigHitOn/src/main/java/com/bighit/on/channel/ChannelDaoImpl.java
@@ -2,15 +2,21 @@ package com.bighit.on.channel;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import com.bighit.on.cmn.DTO;
+
 @Repository("ChannelDaoImpl")
 public class ChannelDaoImpl {
-
+	final static Logger   LOG = LoggerFactory.getLogger(ChannelDaoImpl.class);
+	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	RowMapper rowMapper = new RowMapper<ChannelVO>() {
@@ -89,4 +95,67 @@ public class ChannelDaoImpl {
 		flag = this.jdbcTemplate.update(sb.toString(), args);
 		return flag;
 	}
+	
+	/**
+	 * 채널 단건조회
+	 * @param chLink
+	 * @return
+	 */
+	public ChannelVO doSelectOne(String chLink) {
+		ChannelVO outVO = null;
+		
+		StringBuilder  sb=new StringBuilder();
+		sb.append("SELECT             \n");
+		sb.append("    ch_link,       \n");
+		sb.append("    ws_link,       \n");
+		sb.append("    ch_name,       \n");
+		sb.append("    topic,         \n");
+		sb.append("    ch_description,\n");
+		sb.append("    ch_access,     \n");
+		sb.append("    reg_id,        \n");
+		sb.append("    reg_dt         \n");
+		sb.append("FROM channels      \n");
+		sb.append("WHERE ch_link = ?  \n");
+		
+		Object args[] = {chLink};
+		outVO = (ChannelVO) this.jdbcTemplate.queryForObject(sb.toString(), args,rowMapper);
+		LOG.debug("=doSelectOne outVO="+outVO);
+		
+		return outVO;
+		
+	}
+	
+	public List<ChannelVO> doSelectList(ChannelVO channelVO) {
+		List<ChannelVO> list = null;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT             \n");
+		sb.append("    ch_link,       \n");
+		sb.append("    ws_link,       \n");
+		sb.append("    ch_name,       \n");
+		sb.append("    topic,         \n");
+		sb.append("    ch_description,\n");
+		sb.append("    ch_access,     \n");
+		sb.append("    reg_id,        \n");
+		sb.append("    reg_dt         \n");
+		sb.append("FROM channels      \n");
+		sb.append("WHERE ws_link = ?  \n");
+		
+		LOG.debug("---------------------------");
+		LOG.debug("-sql-\n" + sb.toString());
+		LOG.debug("-param-\n" + channelVO);
+		LOG.debug("---------------------------");
+		
+		list = this.jdbcTemplate.query(sb.toString(),  new Object[] {"%"+channelVO.getWsLink()+"%"}, rowMapper);
+		
+		for(ChannelVO vo : list) {
+			LOG.debug("===========================");
+			LOG.debug("=doSelectList vo="+vo);
+			LOG.debug("===========================");
+		}
+		
+		return list;
+		
+	}
+	
 }
