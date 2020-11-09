9e4cb050dd2825b6c94711e4a70f665836d65680
ehr <ehr@DESKTOP-4TTL0DO>
2020-11-09 15:26:23 +0900

    이상녕

--- BigHitOn/src/main/java/com/bighit/on/channelusers/ChannelUsersDao.java
+++ BigHitOn/src/main/java/com/bighit/on/channelusers/ChannelUsersDao.java
@@ -3,6 +3,7 @@ package com.bighit.on.channelusers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,53 +21,57 @@ public class ChannelUsersDao {
 	final static Logger LOG = LoggerFactory.getLogger(ChannelUsersDao.class);
 	
 	@Autowired
-	JdbcTemplate jdbcTemplate;
-	RowMapper rowMapper = new RowMapper<ChannelUsersVO>() {
-		@Override
-		public ChannelUsersVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-			ChannelUsersVO outVO = new ChannelUsersVO();
-			outVO.setChLink(rs.getString("ch_link"));
-			outVO.setUserSerial(rs.getString("user_serial"));
-			outVO.setNotification(rs.getInt("notification"));
-			return outVO;
-		}
-	};
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	private final String NAMESPACE = "com.bighit.on.channelusers";
+	
+//	RowMapper rowMapper = new RowMapper<ChannelUsersVO>() {
+//		@Override
+//		public ChannelUsersVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//			ChannelUsersVO outVO = new ChannelUsersVO();
+//			outVO.setChLink(rs.getString("ch_link"));
+//			outVO.setUserSerial(rs.getString("user_serial"));
+//			outVO.setNotification(rs.getInt("notification"));
+//			return outVO;
+//		}
+//	};
 	/**
 	 * doInsert
 	 * 
 	 * @param inVO
 	 * @return
 	 */
-	public int doInsert(ChannelUsersVO inVO) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("INSERT INTO channel_users (  \n");
-		sb.append("    ch_link,                 \n");
-		sb.append("    user_serial,             \n");
-		sb.append("    notification             \n");
-		sb.append(") VALUES (                   \n");
-		sb.append("    ?,                       \n");
-		sb.append("    ?,                       \n");
-		sb.append("    ?                        \n");
-		sb.append(")							\n");
-		Object args[] = {inVO.getChLink(),inVO.getUserSerial(),inVO.getNotification() };
-		int flag = this.jdbcTemplate.update(sb.toString(), args);
-		return flag;
-	}
+	public int doInsert(ChannelUsersVO inVO)
+		{
+			LOG.debug("=====================");
+			LOG.debug("=doInsert=");
+			LOG.debug("=====================");
+			String statement = NAMESPACE +".doInsert";
+			LOG.debug("=statement="+statement);
+			LOG.debug("=param ==="+inVO);
+			
+			int flag = sqlSessionTemplate.insert(statement, inVO);
+			LOG.debug("-doInsert flag=" + flag);
+			return flag;		
+		}
 	/**
 	 * do Delete
 	 * @param inVO
 	 * @return
 	 */
-	public int doDelete(ChannelUsersVO inVO) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("DELETE FROM channel_users  \n");
-		sb.append("WHERE                      \n");
-		sb.append("    ch_link = ?            \n");
-		sb.append("    AND user_serial = ?	  \n");
-		Object args[] = {inVO.getChLink(),inVO.getUserSerial()};
-		int flag = this.jdbcTemplate.update(sb.toString(), args);
-		return flag;
-	}
+	public int doDelete(ChannelUsersVO inVO) 
+		{
+			LOG.debug("=====================");
+			LOG.debug("=doInsert=");
+			LOG.debug("=====================");
+			String statement = NAMESPACE +".doDelete";
+			LOG.debug("=statement="+statement);
+			LOG.debug("=param ==="+inVO);
+			
+			int flag = sqlSessionTemplate.delete(statement, inVO);
+			LOG.debug("-doInsert flag=" + flag);
+			return flag;			
+		}
 	/**
 	 * 채널VO 입력시 
 	 * 해당 채널에 걸려있는 모든 행 삭제,
@@ -91,7 +96,7 @@ public class ChannelUsersDao {
 			sb.append("    user_serial = ?	  	  \n");
 			arg = ((UsersVO)dto).getUser_serial();
 		}
-		int flag = this.jdbcTemplate.update(sb.toString(), arg);
+		int flag = this.sqlSessionTemplate.update(sb.toString(), arg);
 		return flag;		
 	}
 	
