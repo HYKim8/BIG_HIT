482945aafea8479bbb130372d985df3a163041f4
xp133 <xp133@DESKTOP-NQKEJR3>
2020-11-09 10:36:10 +0900

    saveThr mybatics 수정

--- BigHitOn/src/main/java/com/bighit/on/save/SaveThrDaoImpl.java
+++ BigHitOn/src/main/java/com/bighit/on/save/SaveThrDaoImpl.java
@@ -4,6 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -11,25 +14,19 @@ import org.springframework.stereotype.Repository;
 
 import com.bighit.on.cmn.DTO;
 import com.bighit.on.cmn.DaoInterface;
+import com.bighit.on.mention.MentionDaoImpl;
+import com.bighit.on.mention.MentionVO;
 import com.bighit.on.reminder.ReminderVO;
 
 @Repository("SaveThrDaoImpl")
 public class SaveThrDaoImpl {
 	
+	final static Logger   LOG = LoggerFactory.getLogger(SaveThrDaoImpl.class);
+	
 	@Autowired
-	JdbcTemplate jdbcTemplate;
-	RowMapper rowMapper = new RowMapper<SaveThrVO>() {
-		@Override
-		public SaveThrVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-			SaveThrVO outVO = new SaveThrVO();
-			
-			outVO.setThrKey(rs.getString("thr_key"));
-			outVO.setRegDt(rs.getString("reg_dt"));
-			outVO.setRegId(rs.getString("reg_id"));
-			
-			return outVO;
-		}
-	};
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	private final String NAMESPACE = "com.bighit.on.save";
 	/**
 	 * 쓰레드 저장 
 	 * @param saveThrVO
@@ -37,23 +34,9 @@ public class SaveThrDaoImpl {
 	 */
 	
 	public int doInsert(SaveThrVO saveThrVO) {
-		int flag = 0;
-		Object[] args = {
-					saveThrVO.getRegId(),
-					saveThrVO.getThrKey()				
-		};
-		StringBuilder sb = new StringBuilder();
-		sb.append("INSERT INTO save_thr (   \n");
-		sb.append("    reg_id,              \n");
-		sb.append("    thr_key,             \n");
-		sb.append("    reg_dt               \n");
-		sb.append(") VALUES (               \n");
-		sb.append("    ?,                   \n");
-		sb.append("    ?,                   \n");
-		sb.append("    sysdate              \n");
-		sb.append(")						\n");
-		
-		flag = this.jdbcTemplate.update(sb.toString(), args);
+		String statement = NAMESPACE +".doInsert";
+		int flag = sqlSessionTemplate.insert(statement, saveThrVO);
+		LOG.debug("-doInsert flag=" + flag);
 		return flag;
 	}
 	/**
@@ -62,17 +45,9 @@ public class SaveThrDaoImpl {
 	 * @return
 	 */
 	public int doDelete(SaveThrVO saveThrVO) {
-		int flag = 0;
-		StringBuilder sb = new StringBuilder();
-		Object[] args = {
-				saveThrVO.getRegId(),
-				saveThrVO.getThrKey()				
-		};
-		sb.append("DELETE FROM save_thr \n");
-		sb.append("WHERE                \n");
-		sb.append("    reg_id = ?       \n");
-		sb.append("    AND thr_key = ?	\n");
-		flag = this.jdbcTemplate.update(sb.toString(), args);
+		String statement = NAMESPACE +".doDelete";
+		int flag = sqlSessionTemplate.delete(statement, saveThrVO);
+		LOG.debug("-doInsert flag=" + flag);
 		return flag;
 	}
 
@@ -86,24 +61,17 @@ public class SaveThrDaoImpl {
 		return null;
 	}
 	
-	public List<SaveThrVO> doSelectList(DTO dto) {
-		SaveThrVO inVO = (SaveThrVO) dto;
-		List<SaveThrVO> outList = null;
-		Object[] args = {
-			inVO.getRegId()
-		};
+	public List<SaveThrVO> doSelectList(SaveThrVO inVO) {
+		String statement = NAMESPACE +".doSelectList";
+		List<SaveThrVO> list = this.sqlSessionTemplate.selectList(statement, inVO);
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append("SELECT                   \n");
-		sb.append("    thr_key,             \n");
-		sb.append("    reg_id,              \n");
-		sb.append("    reg_dt               \n");
-		sb.append("FROM                     \n");
-		sb.append("    save_thr             \n");
-		sb.append("WHERE                    \n");
-		sb.append("    reg_id = ?           \n");
-		outList = this.jdbcTemplate.query(sb.toString(), args, rowMapper);
-		return outList;
+		for(SaveThrVO vo : list) {
+			LOG.debug("===========================");
+			LOG.debug("=doSelectList vo="+vo);
+			LOG.debug("===========================");
+		}
+		
+		return list;
 	}
 
 }
