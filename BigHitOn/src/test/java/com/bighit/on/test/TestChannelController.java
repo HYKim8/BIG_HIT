29e96a1dd6915e264d9d52954f1f54fb5718bab4
isc <isc@DESKTOP-C3NCPKE>
2020-11-11 13:59:01 +0900

    테스트채널컨트롤러

--- BigHitOn/src/test/java/com/bighit/on/test/TestChannelController.java
+++ BigHitOn/src/test/java/com/bighit/on/test/TestChannelController.java
@@ -37,142 +37,130 @@ import com.google.gson.Gson;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)//스프랭 테스트 컨텍스트 프레임워크의 JUnit기능 확장
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml",
-                                   "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"		
-})
+@RunWith(SpringJUnit4ClassRunner.class) // 스프랭 테스트 컨텍스트 프레임워크의 JUnit기능 확장
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml" })
 public class TestChannelController {
-	Logger  LOG = LoggerFactory.getLogger(TestChannelController.class);
-	
+	Logger LOG = LoggerFactory.getLogger(TestChannelController.class);
+
 	@Autowired // 테스트 컨텍스트 프레임워크는 일치하는 컨텍스트를 찾아 DI해 준다.
 	WebApplicationContext webApplicationContext;
-	
+
 	@Autowired
 	ChannelService channelService;
-	
+
 	List<ChannelVO> channels;
-	
+
 	// 브라우저 대신 Mock
 	MockMvc mockMvc;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		channels=Arrays.asList(new ChannelVO("12","1","testCh1","=testch1test=","testspace","1","jhs",""),
-							   new ChannelVO("123","1","testCh1_U","=testch1test_U=","testspace_U","1","jhs_U",""));
+		channels = Arrays.asList(new ChannelVO("12", "1", "testCh1", "=testch1test=", "testspace", "1", "jhs", ""),
+				new ChannelVO("123", "1", "testCh1_U", "=testch1test_U=", "testspace_U", "1", "jhs_U", ""));
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 		LOG.debug("=mockMvc=" + mockMvc);
 		assertThat(mockMvc, is(notNullValue()));
-		
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-	
-	//성공
+
+	// 성공
 	@Test
 	@Ignore
 	public void doInsert() throws Exception {
 		ChannelVO channelVO = channels.get(0);
-		MockHttpServletRequestBuilder createMessage = 
-				 MockMvcRequestBuilders.post("/channel/doInsert.do")
-				 .param("chLink", channelVO.getChLink())
-				 .param("wsLink", channelVO.getWsLink())
-				 .param("chName", channelVO.getChName())
-				 .param("topic", channelVO.getTopic())
-				 .param("chDescription", channelVO.getChDescription())
-				 .param("chAccess", channelVO.getChAccess())
-				 .param("regId", channelVO.getRegId());
-		ResultActions resultActions = mockMvc.perform(createMessage)
-				.andExpect(status().is2xxSuccessful());	
-	
+		MockHttpServletRequestBuilder createMessage = MockMvcRequestBuilders.post("/channel/doInsert.do")
+				.param("chLink", channelVO.getChLink()).param("wsLink", channelVO.getWsLink())
+				.param("chName", channelVO.getChName()).param("topic", channelVO.getTopic())
+				.param("chDescription", channelVO.getChDescription()).param("chAccess", channelVO.getChAccess())
+				.param("regId", channelVO.getRegId());
+		ResultActions resultActions = mockMvc.perform(createMessage).andExpect(status().is2xxSuccessful());
+
 		String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
 		LOG.debug("===========================");
 		LOG.debug("=result=" + result);
 		LOG.debug("===========================");
-		
-		//json -> Message
-		Gson gson=new Gson();
-		
+
+		// json -> Message
+		Gson gson = new Gson();
+
 		Message message = gson.fromJson(result, Message.class);
 		LOG.debug("===========================");
 		LOG.debug("=message=" + message);
 		LOG.debug("===========================");
 	}
-	
-	//성공
+
+	// 성공
 	@Test
 	@Ignore
 	public void doDelete() throws Exception {
 		ChannelVO channelVO = channels.get(0);
-		LOG.debug("channelVO"+channelVO);
-		
-		MockHttpServletRequestBuilder createMessage = 
-				 MockMvcRequestBuilders.post("/channel/doDelete.do")
-				 .param("chLink", channelVO.getChLink());
-		
-		ResultActions resultActions = mockMvc.perform(createMessage)
-				.andExpect(status().is2xxSuccessful());	
-	
+		LOG.debug("channelVO" + channelVO);
+
+		MockHttpServletRequestBuilder createMessage = MockMvcRequestBuilders.post("/channel/doDelete.do")
+				.param("chLink", channelVO.getChLink());
+
+		ResultActions resultActions = mockMvc.perform(createMessage).andExpect(status().is2xxSuccessful());
+
 		String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
 		LOG.debug("===========================");
 		LOG.debug("=result=" + result);
 		LOG.debug("===========================");
-		
-		//json -> Message
-		Gson gson=new Gson();
-		
+
+		// json -> Message
+		Gson gson = new Gson();
+
 		Message message = gson.fromJson(result, Message.class);
 		LOG.debug("===========================");
 		LOG.debug("=message=" + message);
 		LOG.debug("===========================");
 	}
-	
-	//성공
+
+	// 성공
 	@Test
 	public void doSelectOne() throws Exception {
 		ChannelVO channelVO = channels.get(0);
-		
-		MockHttpServletRequestBuilder createMessage = 
-				 MockMvcRequestBuilders.get("/channel/doSelectOne.do")
-				 .param("chLink", channelVO.getChLink());
-		
-		ResultActions resultActions = mockMvc.perform(createMessage)
-				.andExpect(status().is2xxSuccessful());	
-	
+
+		MockHttpServletRequestBuilder createMessage = MockMvcRequestBuilders.get("/channel/doSelectOne.do")
+				.param("chLink", channelVO.getChLink());
+
+		ResultActions resultActions = mockMvc.perform(createMessage).andExpect(status().is2xxSuccessful());
+
 		String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
 		LOG.debug("===========================");
 		LOG.debug("=result=" + result);
 		LOG.debug("===========================");
-		
-		//json -> Message
-		Gson gson=new Gson();
-		
+
+		// json -> Message
+		Gson gson = new Gson();
+
 		Message message = gson.fromJson(result, Message.class);
 		LOG.debug("===========================");
 		LOG.debug("=message=" + message);
 		LOG.debug("===========================");
-		
+
 	}
-	
-	//성공
+
+	// 성공
 	@Test
 	@Ignore
 	public void doSelectList() throws Exception {
 		ChannelVO channelVO = channels.get(0);
-		
-		MockHttpServletRequestBuilder createMessage = 
-				 MockMvcRequestBuilders.get("/channel/doSelectList.do")
-				 .param("wsLink", channelVO.getWsLink());
-		
-		ResultActions resultActions = mockMvc.perform(createMessage)
-				.andExpect(status().is2xxSuccessful());	
-	
+
+		MockHttpServletRequestBuilder createMessage = MockMvcRequestBuilders.get("/channel/doSelectList.do")
+				.param("wsLink", channelVO.getWsLink());
+
+		ResultActions resultActions = mockMvc.perform(createMessage).andExpect(status().is2xxSuccessful());
+
 		String result = resultActions.andDo(print()).andReturn().getResponse().getContentAsString();
 		LOG.debug("===========================");
 		LOG.debug("=result=" + result);
 		LOG.debug("===========================");
-		
+
 	}
-	
+
 }
