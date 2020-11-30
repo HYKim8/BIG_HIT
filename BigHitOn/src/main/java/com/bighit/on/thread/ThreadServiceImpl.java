c874b8c991c318313692e2b8ed24708a48843cf5
osk14741 <66981983+osk14741@users.noreply.github.com>
2020-11-30 20:31:23 +0900

    g

--- BigHitOn/src/main/java/com/bighit/on/thread/ThreadServiceImpl.java
+++ BigHitOn/src/main/java/com/bighit/on/thread/ThreadServiceImpl.java
@@ -8,10 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bighit.on.channel.ChannelVO;
 import com.bighit.on.cmn.Search;
 
 
-@Service
 public class ThreadServiceImpl implements ThreadService {
    final Logger LOG = LoggerFactory.getLogger(this.getClass());
    @Autowired
@@ -24,6 +24,10 @@ public class ThreadServiceImpl implements ThreadService {
       }
       
       return thrDao.doSelectList(search);
+   }
+   
+   public List<ThreadVO> doSelectListIsPinned(ChannelVO channelVO){
+	   return thrDao.doSelectListIsPinned(channelVO);
    }
    
    @Override
